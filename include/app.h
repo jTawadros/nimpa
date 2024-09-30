8608ed360c984cd15efc2818034923ccb6e9db3f
Jonathan Tawadros <jonathanawa>
2024-09-30 09:59:47 -0700

    Initial commit of Nimpa text editor

---
+++ include/app.h
@@ -0,0 +1,13 @@
+#ifndef APP_H
+#define APP_H
+
+class App {
+    public:
+        App();
+        void start();
+    private:
+        void initialize();
+        void main_loop();
+};
+
+#endif // APP_H
